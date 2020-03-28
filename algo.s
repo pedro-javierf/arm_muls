61bcbc4fd8c263990c0ab43577eaa28743eec36b
Pedro Javier <pedro-javierf@users.noreply.github.com>
2020-03-28 13:48:20 +0100

    Update algo.s

--- algo.s
+++ algo.s
@@ -25,7 +25,29 @@ beq zeroReturn
 
 //Multiplication Algorithm
 
+//1. Non-signed multiplication of mantissas
+@get mantissa of A
+mov r4, r0, lsl 9
+lsr r4, 9
 
+@get mantissa of B
+mov r5, r1, lsl 9
+lsr r5, 9
+
+@multiply
+@normalize before multiplication (add the left 1). The mantissa is now 24 bits
+@0x00800000 is the equivalent of bit 24 set.
+orr r4, 0x‭00800000‬
+orr r5, 0x‭00800000‬
+
+@the result is indeed 48 bits
+UMULL r6,r7,r4,r5 
+
+//2. Normalization of the result
+
+//3. Addition of exponents
+
+//4. Sign computation
 
 mov r0, x
 b end
