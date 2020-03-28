9ad48bef14e87a97f9227a1524f29eb8929e7cc7
Pedro Javier <pedro-javierf@users.noreply.github.com>
2020-03-28 14:09:36 +0100

    Update algo.s

--- algo.s
+++ algo.s
@@ -1,7 +1,6 @@
 //arm_muls: single point multiplication algorithm for ARM
 //By: Pedro Javier Fernández
 
-
 .global start
 .extern _stack
 
@@ -41,13 +40,52 @@ orr r4, 0x‭00800000‬
 orr r5, 0x‭00800000‬
 
 @the result is indeed 48 bits
+//    hi lo  A  B
 UMULL r6,r7,r4,r5 
 
 //2. Normalization of the result
+@get the new mantissa
+@Count leading zeros in first 32 bits of the register
+clz r10,r6
+@Shift that amount of zeros plus one
+add r10,r10,#1
+lsl r6,r10
+@Final mantissa is the shifted part logical OR'ed with the low bits of the multiplication
+orr r9, r6,r7
+
 
 //3. Addition of exponents
 
+@ Er = (Ea-127) + (Eb-127) + 127 = Ea + Eb – 127
+
+@get exponent of A
+mov r4, r0, lsr #23
+and r4, r4, 0x000000FF @remove sign bit
+
+@get exponent of B
+mov r5, r0, lsr #23
+and r5, r5, 0x000000FF @remove sign bit
+
+@Ea + Eb – 127
+add r10, r4, r5
+sub r10, r10, 127
+
 //4. Sign computation
+mov r4, r0, lsr #31 @get bit of A
+mov r5, r1, lsr #31 @get bit of B
+
+eor r11, r4, r5
+
+//5. Putting everything together
+
+@mantissa in place
+mov r0, r9 
+
+@exponent in place
+orr r0, r10, lsl 23
+
+@sign bit in place
+orr r0, r11, lsl 31
 
 mov r0, x
 b end
